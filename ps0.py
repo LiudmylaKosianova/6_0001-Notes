52b4882a6db5d1a9288372283362d0a2e6a53c33
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-30 20:43:57 +0100

    problem set 0 start

---
+++ ps0.py
@@ -0,0 +1,10 @@
+#Raising a number to a power and taking a logarithm
+
+user_in = input("Enter number x: ")
+try:
+    intX = int (user_in)
+except ValueError :
+    print("Not a valid number")
+    
+user_in = input("Enter number y:")
+
